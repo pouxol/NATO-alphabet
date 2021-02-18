1561994086b2229f18b65496a602d3f566717a3b
pouxol <41949525+pouxol@users.noreply.github.com>
2021-02-18 20:09:32 +0100

    added error handling

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ alphabet_dict = {row.letter: row.code for (index, row) in alphabet.iterrows()}
 # Create a list of the phonetic code words from a word that the user inputs.
 input_word = input("Input word: ").upper()
 
-words = [alphabet_dict[letter] for letter in input_word]
+words = []
+for letter in input_word:
+    try:
+        words.append(alphabet_dict[letter])
+    except KeyError:
+        print("You are only allowed to input letters.")
 
 print(words)
